518032d5d0ca12b93897dab0ea98915ace630795
AFCMS <afcm.contact@gmail.com>
2021-11-22 15:43:57 +0100

    add some files

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
---waystones = {}
+minetest.log("action", "[waystones] loading...")
+
+local modpath = minetest.get_modpath("waystones")
+
+waystones = {}
+
+
+dofile(modpath.."/items/warp_stone.lua")
+dofile(modpath.."/crafting.lua")
+
+
+minetest.log("action", "[waystones] loaded succesfully")
---
+++ items/warp_stone.lua
@@ -0,0 +1,4 @@
+minetest.register_tool("waystones:warp_stone", {
+	description = "Warp Stone",
+	inventory_image = "waystones_warp_stone.png",
+})
